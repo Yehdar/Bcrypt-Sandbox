5262c8811fd79ea8c913e7d5273ace61c13ad331
Yehdar <p.radhey06@gmail.com>
2023-05-31 11:51:32 -0400

    Fixed by replacing PHP salt with NodeJS salt

--- index.mjs
+++ index.mjs
@@ -10,9 +10,10 @@ const hashed = bcrypt.hashSync(userInputPassword, 10);
 
 // cool built-in method in bcrypt that does a boolean to verify password and its supposed hash
 // since u cant reverse a hash, the method hashes the original password and then compares to the hash
-const matchTest = bcrypt.compareSync(userInputPassword, passwordHashed.hash1);
+const matchTest = bcrypt.compareSync(userInputPassword, passwordHashed.hash1.replace('$2y$','$2b$'));
 
 console.log(hashed);
 // $2b$10$BQS4SVZIgCHG70igI13fvOs1p2hvzPIdW6RXMGkSMpL2oAFflDEyC
 console.log(passwordHashed.hash1);
 // $2y$10$odjvTUH86JeNhe.JVd4W7.otn0mPP4iMHXpeMgLza/eRfqC.iXmAa
+console.log(matchTest)
